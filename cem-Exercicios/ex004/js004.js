d2dfd347943ba388bc3f2eedf835bccbbba016dd
Welder Oliveira Viana de Araújo <75491768+welderova@users.noreply.github.com>
2023-02-27 15:12:33 -0300

    27/02/2023
    atualizações para ajuste da pagina e projetos

---
+++ cem-Exercicios/ex004/js004.js
@@ -0,0 +1,9 @@
+var n1 = document.querySelector('.n1');
+var n2 = document.querySelector('.n2');
+var somar = document.querySelector('span')
+
+
+
+function somar() {
+    somar.innerHTML = parseInt(n1.value) + parseInt(n2.value);
+}
